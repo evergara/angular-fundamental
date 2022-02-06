9ff03a27778eac3f47bcbd533f2bf17b62dd9d6c
Emerson Vergara <ee-web@hotmail.com>
2022-02-05 23:39:32 -0500

    grid css

---
+++ src/app/feature/tarea3/tarea3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea3',
+  templateUrl: './tarea3.component.html',
+  styleUrls: ['./tarea3.component.scss']
+})
+export class Tarea3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
