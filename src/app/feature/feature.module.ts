ac61233408e90fad6e405277fba63b8c91bf55df
Emerson Vergara <ee-web@hotmail.com>
2022-04-02 05:08:57 -0500

    Adecuando proyecto base

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -4,22 +4,13 @@ import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { HomeComponent } from './home/home.component';
-import { Tarea1Component } from './tarea1/tarea1.component';
-import { ProyectoComponent } from './proyecto/proyecto.component';
-import { Proyecto2Component } from './proyecto2/proyecto2.component';
-import { Tarea2Component } from './tarea2/tarea2.component';
-import { Tarea3Component } from './tarea3/tarea3.component';
+
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    Tarea1Component,
-    ProyectoComponent,
-    Proyecto2Component,
-    Tarea2Component,
-    Tarea3Component
   ],
   imports: [
     CommonModule,
