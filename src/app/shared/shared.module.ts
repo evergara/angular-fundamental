acb5d8107d898b8061d3a607d75b5f636e6430e4
Emerson Vergara <ee-web@hotmail.com>
2022-04-07 21:59:09 -0500

    onfiguracion Quitamos componentes del main

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavComponent } from './components/nav/nav.component';
-import { LogoComponent } from './components/logo/logo.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent,
-    NavComponent,
-    LogoComponent,
-    SidebarComponent,
-  ],
-  exports: [
-    FooterComponent,
-    HeaderComponent,
-    NavComponent,
-    LogoComponent,
-    SidebarComponent,
-  ],
+  declarations: [SidebarComponent],
+  exports: [SidebarComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
