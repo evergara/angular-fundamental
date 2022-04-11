7b0ce13f965aaa5ea034e33ac586b844b1978e63
Emerson Vergara <ee-web@hotmail.com>
2022-04-11 07:42:58 -0500

    Configuration: pasar la logica del menu al sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 
-
-
 @NgModule({
-  declarations: [
-    SidebarComponent
-  ],
-  exports:[
-    SidebarComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SidebarComponent],
+  exports: [SidebarComponent],
+  imports: [CommonModule, RouterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
