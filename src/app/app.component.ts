23bb8aea671585d9fbe7c664f600ba6a6fc1b1e6
Emerson Vergara <ee-web@hotmail.com>
2022-04-13 11:44:58 -0500

    Introduccion a los pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-fundamental';
+  title: string = 'angular-fundamental';
+  value: number = 10000;
+  person = {
+    name: 'evergara',
+  };
+
+  showName(): void {
+    console.log(this.title);
+    console.log(this.value);
+    console.log(this.person);
+  }
 }
