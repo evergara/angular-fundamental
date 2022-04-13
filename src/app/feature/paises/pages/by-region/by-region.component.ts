c986d9b9f6c2a4d31cd98c22b85ee26ae09ed366
Emerson Vergara <ee-web@hotmail.com>
2022-04-12 20:09:38 -0500

    uso de ngClass, class y [class] y clases condicionadas

--- src/app/feature/paises/pages/by-region/by-region.component.ts
+++ src/app/feature/paises/pages/by-region/by-region.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../shared/services/countries.service';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
-  styleUrls: ['./by-region.component.scss']
+  styleUrls: ['./by-region.component.scss'],
 })
-export class ByRegionComponent implements OnInit {
+export class ByRegionComponent {
+  regions: string[];
+  regionActiva: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private countriesService: CountriesService) {
+    this.regions = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+    this.regionActiva = '';
   }
 
+  activarRegion(region: string): void {
+    this.regionActiva = region;
+    alert(region);
+
+    //TODO: llamar servicios
+
+    this.countriesService.searchRegion(region).subscribe((countries) => {
+      console.log(countries);
+    });
+  }
+
+  getClassCSS(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
+  }
 }
