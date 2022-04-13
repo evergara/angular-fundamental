d79b081ebc14e1a82362265bb1de47190e7cd2c7
Emerson Vergara <ee-web@hotmail.com>
2022-04-13 10:27:44 -0500

    Configurando Jest cambios antes de comenzar

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
   },
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  collectCoverage: true,// Nos crea la carpeta Coverage cada vez que se ejecutan las pruebas
+  coverageDirectory: '<rootDir>/src/tests/coverage' // Donde lo vas  a dejar
 };
