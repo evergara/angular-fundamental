f1414c07650af916e6239dc9a7fc0c8a516126f6
Emerson Vergara <ee-web@hotmail.com>
2022-04-18 13:25:19 -0500

    Organizando:  quitanto todo para empezar con la rama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FeatureModule } from './feature/feature.module';
 
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./feature/feature.module').then(mod => mod.FeatureModule),
-  }
-];
-
+const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
