7cb20a37165293088a27ec8fb11ea7a92227bb97
Emerson Vergara <ee-web@hotmail.com>
2022-05-03 04:40:17 -0500

    Rutas [routerLink]="['/heroes', heroe.id]" const id =this.activatedRoute.snapshot.paramMap.get('id');

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getIdHeroe();
   }
 
+  getIdHeroe(): void{
+    const id =this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(id);
+  }
 }
