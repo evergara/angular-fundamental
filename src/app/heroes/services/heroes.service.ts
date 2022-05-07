2d8bb2ed7442e459e2b8c6d68796cebd5a24a1ff
Emerson Vergara <ee-web@hotmail.com>
2022-05-07 04:51:56 -0500

    Centralizando las rutas de la api

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Heroe } from '../interfaces/heroe';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
-  
+  private baseUrl: string = environment.apiHeroes;
+
   constructor(private http: HttpClient) {}
 
   heroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
   heroeById(heroeId: string): Observable<Heroe> {
-    return this.http.get<Heroe>('http://localhost:3000/heroes/'+heroeId)    
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${heroeId}`);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiHeroes: '#{Heroes.Url}#',
 };
